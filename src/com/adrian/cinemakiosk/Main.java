96dcdb2f52ac1e9cb6851e47a95798b092b0f4c8
Adretanoo <adriantegzza@gmail.com>
2025-01-27 22:48:02 +0200

    renewal

--- src/com/adrian/cinemakiosk/Main.java
+++ src/com/adrian/cinemakiosk/Main.java
@@ -1,42 +1,43 @@
 package com.adrian.cinemakiosk;
 
 import com.adrian.cinemakiosk.domain.entitys.impl.User;
-import com.github.javafaker.Faker;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import com.adrian.cinemakiosk.domain.validators.UserValidator;
 
 public class Main {
 
+//    public static void main(String[] args) {
+//        Faker faker = new Faker();
+//        List<User> users = new ArrayList<>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            User user = new User(
+//                i + 1,
+//                faker.name().fullName(),
+//                faker.internet().emailAddress(),
+//                faker.internet().password(),
+//                faker.bool().bool() ? "user" : "admin"
+//            );
+//            users.add(user);
+//
+//            System.out.println(user);
+//        }
+//        saveToJsonFile("data/users.json", users);
+//    }
+//
+//    private static void saveToJsonFile(String filePath, Object data) {
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        try (FileWriter writer = new FileWriter(filePath)) {
+//            gson.toJson(data, writer);
+//            System.out.println("Дані збережено до файлу: " + filePath);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
     public static void main(String[] args) {
-        Faker faker = new Faker();
-        List<User> users = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            User user = new User(
-                i + 1,
-                faker.name().fullName(),
-                faker.internet().emailAddress(),
-                faker.internet().password(),
-                faker.bool().bool() ? "user" : "admin"
-            );
-            users.add(user);
-
-            System.out.println(user);
-        }
-        saveToJsonFile("data/users.json", users);
+        User user = new User(1, "", "", "", "");
+        UserValidator.validate(user);
     }
 
-    private static void saveToJsonFile(String filePath, Object data) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try (FileWriter writer = new FileWriter(filePath)) {
-            gson.toJson(data, writer);
-            System.out.println("Дані збережено до файлу: " + filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
